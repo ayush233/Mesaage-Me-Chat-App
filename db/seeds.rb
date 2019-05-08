6a2712fdbdefeacbee1f259777fe4e99a05139ce
Ayush Agrawal <autarchy14@gmail.com>
2019-05-08 19:14:51 +0000

    Added login page, links for chatroom in navbar,created Users and Messages model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Ram", password:"password")
+User.create(username: "Shyam", password:"password")
+User.create(username: "Ghanshyam", password:"password")
+User.create(username: "Pappu", password:"password")
+User.create(username: "Chunnu", password:"password")
+
